9ef32578ba36ef7efb47fd96e10c2ca9cb9b9e03
Chip Hogg <chogg@aurora.tech>
2022-12-01 21:28:38 -0500

    Add automatic tests for ready-made single files (#24)
    We will plan to link directly to these files on the "installation" page
    of our doc website.  It's important to check, automatically, that
    including these files lets us do all the stuff we want to do with the Au
    library.
    
    To achieve this, we create a `cc_library` to go with each genrule.  We
    then add a `cc_test` rule for each of these libraries to put them
    through their paces.  Common test cases are factored out into a separate
    file for maintainability.
    
    What do we "expect" from our single-file installations?
    
    1. Basic Quantity/QuantityPoint functionality.
    2. Prefixes.
    3. Math functions.
    4. Base units for each dimension, including dimensionless (Unos).
    
    By creating these tests, we uncovered that several of these pieces of
    functionality were missing.  We weren't previously including `prefix.hh`
    (and this PR fixes that).  We also weren't including most of the units,
    ever since #17 landed and split them into their own files.

---
+++ release/au_noio_hh_test.cc
@@ -0,0 +1,13 @@
+// Copyright 2022 Aurora Operations, Inc.
+
+#include <chrono>
+
+#include "docs/au_noio.hh"
+#include "gtest/gtest.h"
+
+// clang-format off
+//
+// This file textually includes all of the test cases which every ready-made
+// single-file release should pass.  It needs to go after all other includes.
+#include "release/common_test_cases.hh"
+// clang-format on
