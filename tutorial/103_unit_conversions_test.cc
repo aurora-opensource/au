6c7209627b3c742cf369470a9f29fe59bf57d4b1
Chip Hogg <chogg@aurora.tech>
2023-01-27 13:19:21 -0500

    Add Au 103 tutorial: unit conversions (#78)
    We follow the same pattern as earlier tutorials.

---
+++ tutorial/103_unit_conversions_test.cc
@@ -0,0 +1,93 @@
+// Copyright 2023 Aurora Operations, Inc.
+
+#include "au/testing.hh"
+#include "au/units/degrees.hh"
+#include "au/units/hours.hh"
+#include "au/units/meters.hh"
+#include "au/units/miles.hh"
+#include "au/units/radians.hh"
+#include "au/units/seconds.hh"
+#include "gtest/gtest.h"
+
+// For testing/tutorial purposes.
+using namespace au;
+
+// Replace this constant with an appropriate conversion function, wherever it occurs.
+constexpr auto PLACEHOLDER = ZERO;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// EXERCISE 1
+//
+// Migrate the following unit conversions to an inline Au-based solution.  Clean up the parts you no
+// longer need.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+TEST(AdHocConversions, DegreesToRadians) {
+    constexpr double angle_deg = 135.0;
+
+    constexpr double RAD_PER_DEG = M_PI / 180.0;
+
+    // TODO: replace `angle_rad` computation with an ad hoc conversion, using Au.
+    constexpr double angle_rad = angle_deg * RAD_PER_DEG;
+
+    EXPECT_DOUBLE_EQ(angle_rad, 3.0 * M_PI / 4.0);
+}
+
+TEST(AdHocConversions, MilesPerHourToMetersPerSecond) {
+    constexpr double speed_mph = 65.0;
+
+    // Carefully compute conversion factor manually.
+    constexpr double M_PER_CM = 0.01;
+    constexpr double CM_PER_INCH = 2.54;
+    constexpr double INCHES_PER_FEET = 12.0;
+    constexpr double FEET_PER_MILE = 5280.0;
+    constexpr double M_PER_MILE = M_PER_CM * CM_PER_INCH * INCHES_PER_FEET * FEET_PER_MILE;
+
+    constexpr double S_PER_H = 3600.0;
+
+    constexpr double MPS_PER_MPH = M_PER_MILE / S_PER_H;
+
+    // TODO: replace `speed_mps` computation with an ad hoc conversion, using Au.
+    constexpr double speed_mps = speed_mph * MPS_PER_MPH;
+
+    EXPECT_DOUBLE_EQ(speed_mps, 29.0576);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// EXERCISE 2
+//
+// Scroll down to the function `decompose_height()`, and fill in the `PLACEHOLDER` instances with
+// correct expressions that will make the test below pass.
+//
+// You may find it useful to use the explicit-Rep overload to force a conversion that is _generally_
+// risky, but in _this_ instance is known to be OK.  You should not need to use floating point
+// numbers at all.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct Height {
+    QuantityU32<Feet> feet;
+    QuantityU32<Inches> inches;
+};
+
+// This makes the tests easier to write.
+bool operator==(Height a, Height b) { return (a.feet == b.feet) && (a.inches == b.inches); }
+
+// This makes the tests easier to read when they fail.
+std::ostream &operator<<(std::ostream &out, const Height &h) {
+    out << h.feet << " " << h.inches;
+    return out;
+}
+
+// Decompose a height, given in inches, into the largest whole number of feet, plus the leftover
+// inches.  For example, `inches(17)` would be decomposed into `Height{feet(1), inches(5)}`.
+Height decompose_height(QuantityU32<Inches> total_height) {
+    Height h;
+    h.feet = PLACEHOLDER;
+    h.inches = PLACEHOLDER;
+    return h;
+}
+
+TEST(Height, DecomposesCorrectly) {
+    EXPECT_EQ(decompose_height(inches(60)), (Height{feet(5), inches(0)}));
+    EXPECT_EQ(decompose_height(inches(83)), (Height{feet(6), inches(11)}));
+}
