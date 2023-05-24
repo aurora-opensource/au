b29c2a3431c2cdc6b148a73b7d76fd05daaf9903
Chip Hogg <chogg@aurora.tech>
2023-05-24 08:52:02 -0400

    Enable `//:mike` to work without direnv (#133)
    This should fix the problem we found on our first commit to main:
    
    https://github.com/aurora-opensource/au/actions/runs/5059742808/jobs/9081711357
    
    I tested this locally by running `direnv deny`, and iterating on the
    script until `tools/bin/bazel run //:mike -- deploy temp` worked.
    
    I also tested this remotely with the commit
    6622fccf1bc9ca5d8d93318670bc28d432590532.  The associated job succeeded:
    
    https://github.com/aurora-opensource/au/actions/runs/5063907743/jobs/9090995654
    It created commit 865c862cefd5d4df7bacb6ccc1e7cec483a403d1 on branch
    `gh-pages`.

--- mike_bin.py
+++ mike_bin.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import re
 import subprocess
 import sys
@@ -19,19 +20,18 @@ import sys
 from unittest import mock
 import mike.driver
 import mike.mkdocs_utils
+from mike.mkdocs_utils import docs_version_var
+
+
+# We need to monkey patch any function in mike which calls `mkdocs` via its
+# command line interface.  Fortunately, there are only two: `version` and
+# `build`.
 
 
 def override_version():
-    """Retrieve the version in a way that's agnostic to the name of the command.
-
-    Since the `mkdocs` executable isn't installed on our machine, but instead is
-    run through bazel, it turns out that _our_ mkdocs executable thinks it has a
-    different name (since we use a wrapper script).  But mike assumes both that
-    we can call `mkdocs`, _and_ that it thinks its own name is `mkdocs` when we
-    do.
-    """
+    """Override mike's native logic for retrieving the version."""
     output = subprocess.run(
-        ["mkdocs", "--version"],
+        ["update_docs", "--version"],
         check=True,
         stdout=subprocess.PIPE,
         universal_newlines=True,
@@ -47,6 +47,32 @@ def override_version():
     return m.group(1)
 
 
+def override_build(config_file, version, verbose=True):
+    """Override mike's native logic for building the docs."""
+    # Original line:
+    #
+    #     command = (
+    #         ['mkdocs', 'build', '--clean'] +
+    #         (['--config-file', config_file] if config_file else [])
+    #     )
+    #
+    # Changed version (reformatted with Black):
+    command = ["update_docs", "build", "--clean"] + (
+        ["--config-file", config_file] if config_file else []
+    )
+
+    env = os.environ.copy()
+    env[docs_version_var] = version
+
+    output = None if verbose else subprocess.DEVNULL
+    subprocess.run(command, check=True, env=env, stdout=output, stderr=output)
+
+
 if __name__ == "__main__":
+    # `update_docs` is our name for `mkdocs`.  It will be found in the same folder as this script.
+    # We need to add this folder to the PATH.
+    os.environ["PATH"] = f"{os.path.join(os.getcwd())}:{os.environ['PATH']}"
+
     with mock.patch("mike.mkdocs_utils.version", override_version):
-        sys.exit(mike.driver.main())
+        with mock.patch("mike.mkdocs_utils.build", override_build):
+            sys.exit(mike.driver.main())
