2103502d74055f732821299019e0e552ed6dce39
Chip Hogg <chogg@aurora.tech>
2023-07-08 11:56:47 -0400

    Try compiling with MSVC

---
+++ single-file-test.cc
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+
+#include "bazel-bin/docs/au.hh"
+
+using namespace au;
+
+template <typename ExpectedT, typename ActualT>
+bool expect_equal(ExpectedT expected, ActualT actual) {
+    if (expected != actual) {
+        std::cerr << "Failure!  Expected (" << expected << "); Actual (" << actual << ")"
+                  << std::endl;
+        return false;
+    }
+    return true;
+}
+
+int main(int argc, char **argv) {
+    const std::vector<bool> results{
+        {
+            expect_equal((meters / second)(5) * seconds(6), meters(30)),
+        },
+    };
+    return std::all_of(std::begin(results), std::end(results), [](auto x) { return x; }) ? 0 : 1;
+}
