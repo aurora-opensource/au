e8ea564846f340307c0d51e49c597c37cb1f30b7
Chip Hogg <chogg@aurora.tech>
2024-10-30 18:10:10 -0400

    Support Quantity non-type template parameters
    Technically, we can't get non-type template parameters of custom class
    type until C++20.  But for a `Quantity<U, R>` whose rep `R` is integral,
    we can get the next best thing!
    
    This PR introduces a public member typedef, `Quantity<U, R>::NTTP`,
    which _can_ be used as a template parameter, because it's an
    enumeration.  We support _bidirectional, implicit_ conversion between
    `Quantity<U, R>` and `Quantity<U, R>::NTTP`, as long as there's an exact
    match for `U` and `R`.  In all other cases, users must use the usual
    `Quantity` conversion operators to get it into the right type.  They can
    also explicitly request conversion from the NTTP to the `Quantity` by
    passing the former to `from_nttp(...)`.
    
    It's a niche use case, but it's finally possible to use a `Quantity` as
    a template parameter in a unit-safe way!  There just was no other
    comparable solution before this.
    
    Compile time measurements are ongoing, but so far they suggest a small
    but nonzero penalty.  It appears that it varies from file to file, but
    is generally less than 50 ms.  I think that's an OK price to pay for
    this feature.
    
    Fixes #316.

--- au/code/au/quantity.hh
+++ au/code/au/quantity.hh
@@ -385,6 +385,34 @@ class Quantity {
             CorrespondingQuantityT<T>{*this}.in(typename CorrespondingQuantity<T>::Unit{}));
     }
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Pre-C++20 Non-Type Template Parameter (NTTP) functionality.
+    //
+    // If `Rep` is a built in integral type, then `Quantity::NTTP` can be used as a template
+    // parameter.
+
+    enum class NTTP : std::conditional_t<std::is_integral<Rep>::value, Rep, bool> {
+        ENUM_VALUES_ARE_UNUSED
+    };
+
+    constexpr Quantity(NTTP val) : value_{static_cast<Rep>(val)} {
+        static_assert(std::is_integral<Rep>::value,
+                      "NTTP functionality only works when rep is built-in integral type");
+    }
+
+    constexpr operator NTTP() const {
+        static_assert(std::is_integral<Rep>::value,
+                      "NTTP functionality only works when rep is built-in integral type");
+        return static_cast<NTTP>(value_);
+    }
+
+    template <typename C, C x = C::ENUM_VALUES_ARE_UNUSED>
+    constexpr operator C() const = delete;
+    // If you got here ^^^, then you need to do your unit conversion **manually**.  Check the type
+    // of the template parameter, and convert it to that same unit and rep.
+
+    friend constexpr Quantity from_nttp(NTTP val) { return val; }
+
  private:
     template <typename OtherUnit, typename OtherRep>
     static constexpr void warn_if_integer_division() {
