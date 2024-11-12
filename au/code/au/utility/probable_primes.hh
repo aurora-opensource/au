d5165f98797b7e1f85268e20c450b950f72539e6
Chip Hogg <chogg@aurora.tech>
2024-11-12 12:37:06 -0500

    Add Miller-Rabin probable prime algorithm (#321)
    We put this in a new file, `probable_primes.hh`.  Algorithms in this
    class can return one of three outcomes:
    
    - "composite" for numbers that are definitely composite
    - "probably prime" for _all_ primes, and possibly _some_ composite
      numbers
    - "bad input" for numbers that aren't composite or prime (0 or 1), or
      simply aren't appropriate inputs for that particular algorithm.
    
    The wikipedia page[1] gives a good overview of the algorithm.
    
    How do we know that the implementation is correct?  By these main
    strategies:
    
    - [x] Miller-Rabin should mark _every_ prime number as "probably prime",
      so test a large number of known primes, each with a variety of bases.
    
    - [x] Test all (odd) inputs up to a certain maximum, making sure that it
      marks every prime as "probably prime", _every pseudoprime to that
      base_ as "probably prime", and every other number as "composite".
    
    - [x] Test a list of known pseudoprimes for a few given bases, and make
      sure that these are all marked as "probably prime" for that base.
      (Making sure that we make the "right mistakes" is a good way to build
      confidence that we implemented the algorithm correctly.)
    
    - [x] Test an extremely large prime (close to the `uint64_t` limit), to
      check that overflow hasn't crept into any of our calculations.
    
    Given that the implementation passes all of these tests, it seems very
    likely that our implementation is correct.
    
    Helps #217.
    
    [1] https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test

---
+++ au/code/au/utility/probable_primes.hh
@@ -0,0 +1,86 @@
+// Copyright 2024 Aurora Operations, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "au/utility/mod.hh"
+
+namespace au {
+namespace detail {
+
+//
+// The possible results of a probable prime test.
+//
+enum class PrimeResult {
+    COMPOSITE,
+    PROBABLY_PRIME,
+    BAD_INPUT,
+};
+
+//
+// Decompose a number by factoring out all powers of 2: `n = 2^power_of_two * odd_remainder`.
+//
+struct NumberDecomposition {
+    uint64_t power_of_two;
+    uint64_t odd_remainder;
+};
+
+//
+// Express any positive `n` as `(2^s * d)`, where `d` is odd.
+//
+// Preconditions: `n` is positive.
+constexpr NumberDecomposition decompose(uint64_t n) {
+    NumberDecomposition result{0u, n};
+    while (result.odd_remainder % 2u == 0u) {
+        result.odd_remainder /= 2u;
+        ++result.power_of_two;
+    }
+    return result;
+}
+
+//
+// Perform a Miller-Rabin primality test on `n` using base `a`.
+//
+// Preconditions: `n` is odd, and at least as big as `a + 2`.  Also, `2` is the smallest allowable
+// value for `a`.  We will return `BAD_INPUT` if these preconditions are violated.  Otherwise, we
+// will return `PROBABLY_PRIME` for all prime inputs, and also all composite inputs which are
+// pseudoprime to base `a`, returning `COMPOSITE` for all other inputs (which are definitely known
+// to be composite).
+//
+constexpr PrimeResult miller_rabin(std::size_t a, uint64_t n) {
+    if (a < 2u || n < a + 2u || n % 2u == 0u) {
+        return PrimeResult::BAD_INPUT;
+    }
+
+    const auto params = decompose(n - 1u);
+    const auto &s = params.power_of_two;
+    const auto &d = params.odd_remainder;
+
+    uint64_t x = pow_mod(a, d, n);
+    if (x == 1u) {
+        return PrimeResult::PROBABLY_PRIME;
+    }
+
+    const auto minus_one = n - 1u;
+    for (auto r = 0u; r < s; ++r) {
+        if (x == minus_one) {
+            return PrimeResult::PROBABLY_PRIME;
+        }
+        x = mul_mod(x, x, n);
+    }
+    return PrimeResult::COMPOSITE;
+}
+
+}  // namespace detail
+}  // namespace au
