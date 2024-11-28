f9e826e40b90a111dca5a6cb2bcebc3eb426eb70
Chip Hogg <chogg@aurora.tech>
2024-11-28 10:27:08 -0500

    Add machinery for built-in constants, such as `c`
    For Au's built-in constants, we follow the exact same policies as for
    units, including:
    
    - A new include folder, `"au/constants/..."`
    - A new target, `"//au:constants"`, which globs headers from that folder
    - Corresponding unit tests
    - Inclusion in the single-file script by individual names
    - An `--all-constants` option for the single-file script
    
    We _don't_ provide `_fwd.hh` files, because there's nothing we could
    really forward declare.
    
    Constant objects are defined with spelled-out names in `ALL_CAPS`
    format.  The corresponding file is the snake-case version.  This keeps
    the constant itself unambiguous.  We expect end users to actually use
    them in the following manner:
    
    ```cpp
    constexpr auto c = au::SPEED_OF_LIGHT;
    ```
    
    Finally, we now mention new constants in the release notes.
    
    Helps #90.  Remaining work includes adding more constants, and adding
    documentation.

---
+++ au/code/au/constants/speed_of_light.hh
@@ -0,0 +1,38 @@
+// Copyright 2024 Aurora Operations, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "au/constant.hh"
+#include "au/units/meters.hh"
+#include "au/units/seconds.hh"
+
+namespace au {
+
+namespace detail {
+// DO NOT follow this pattern to define your own units.  This is for library-defined units.
+// Instead, follow instructions at (https://aurora-opensource.github.io/au/main/howto/new-units/).
+template <typename T>
+struct SpeedOfLightLabel {
+    static constexpr const char label[] = "c";
+};
+template <typename T>
+constexpr const char SpeedOfLightLabel<T>::label[];
+struct SpeedOfLightUnit : decltype(Meters{} / Seconds{} * mag<299'792'458>()),
+                          SpeedOfLightLabel<void> {
+    using SpeedOfLightLabel<void>::label;
+};
+}  // namespace detail
+
+constexpr auto SPEED_OF_LIGHT = make_constant(detail::SpeedOfLightUnit{});
+
+}  // namespace au
