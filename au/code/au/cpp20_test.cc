04ef65129b5a609ef483d7eb0d634ba3ebdb5694
Chip Hogg <chogg@aurora.tech>
2024-12-10 16:54:30 -0500

    Include <compare>

--- au/code/au/cpp20_test.cc
+++ au/code/au/cpp20_test.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <compare>
+
 #include "au/quantity.hh"
 #include "au/quantity_point.hh"
 #include "au/testing.hh"
