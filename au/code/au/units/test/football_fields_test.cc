311bfae9c9e1963b0fae17d01eddce3976438464
Chip Hogg <chogg@aurora.tech>
2025-05-02 10:31:42 -0400

    Fix build

--- au/code/au/units/test/football_fields_test.cc
+++ au/code/au/units/test/football_fields_test.cc
@@ -23,7 +23,7 @@
 namespace au {
 
 using ::testing::Eq;
-using ::testing::Lt;
+using ::testing::Gt;
 
 TEST(FootballFields, HasExpectedLabel) { expect_label<FootballFields>("ftbl_fld"); }
 
@@ -31,12 +31,12 @@ TEST(FootballFields, HasCorrectQuantityRelationshipWithYards) {
     EXPECT_THAT(football_fields(1), Eq(yards(100)));
 }
 
-TEST(FootballFields, FourFootballFieldsIsLessThanKnownFirstlightLidarRange) {
+TEST(FootballFields, KnownLidarRangeIsAtLeastFourFootballFields) {
     // https://blog.aurora.tech/progress/firstlight-lidar-on-a-chip
     constexpr auto lidar_range_minimum = meters(400);
 
     // https://ir.aurora.tech/news-events/press-releases/detail/119/aurora-begins-commercial-driverless-trucking-in-texas
-    EXPECT_THAT(lidar_range_minimum, Gt(football_fields(4));
+    EXPECT_THAT(lidar_range_minimum, Gt(football_fields(4)));
 }
 
 TEST(FootballFields, HasExpectedSymbol) {
