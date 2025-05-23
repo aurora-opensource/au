c0a57f5296bfcb8abe70bf27fcc65c2e29aa7a7d
Michael Hordijk <hordijk@aurora.tech>
2025-05-23 13:46:48 -0400

    Convert release and compatibility to matcher syntax (#435)
    This converts calls sites in our release and compatibility testing code.
    
    Partial implementation for #404.
    
    Co-authored-by: Chip Hogg <chogg@aurora.tech>

--- release/au_hh_test.cc
+++ release/au_hh_test.cc
@@ -15,6 +15,7 @@
 #include <chrono>
 
 #include "docs/au.hh"
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 // clang-format off
@@ -26,25 +27,29 @@
 
 namespace au {
 
+using ::testing::StrEq;
+
 namespace {
+
 template <typename T>
 std::string stream_to_string(const T &x) {
     std::ostringstream oss;
     oss << x;
     return oss.str();
 }
+
 }  // namespace
 
 TEST(AuHh, PrintsValueAndUnitLabel) {
-    EXPECT_EQ(stream_to_string(meters(3)), "3 m");
-    EXPECT_EQ(stream_to_string((meters / milli(second))(1.25)), "1.25 m / ms");
+    EXPECT_THAT(stream_to_string(meters(3)), StrEq("3 m"));
+    EXPECT_THAT(stream_to_string((meters / milli(second))(1.25)), StrEq("1.25 m / ms"));
 }
 
 TEST(AuHh, DistinguishesPointFromQuantityByAtSign) {
-    EXPECT_EQ(stream_to_string(kelvins(20)), "20 K");
-    EXPECT_EQ(stream_to_string(kelvins_pt(20)), "@(20 K)");
+    EXPECT_THAT(stream_to_string(kelvins(20)), StrEq("20 K"));
+    EXPECT_THAT(stream_to_string(kelvins_pt(20)), StrEq("@(20 K)"));
 }
 
-TEST(AuHh, PrintsZero) { EXPECT_EQ(stream_to_string(ZERO), "0"); }
+TEST(AuHh, PrintsZero) { EXPECT_THAT(stream_to_string(ZERO), StrEq("0")); }
 
 }  // namespace au
