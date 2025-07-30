e027ce61c52cca0f915e4ee8837a1aa63174c266
Chip Hogg <chogg@aurora.tech>
2025-07-30 15:40:15 -0400

    Add support for {fmt} (#491)
    The structure of this support was much more challenging than I had
    anticipated.  I _wanted_ to get out-of-the-box support for anyone who
    included `"fmt/format.h"`, by forward declaring `fmt::formatter`, and
    then specializing it.  This doesn't work because `fmt::formatter` is
    actually `fmt::v11::formatter` (currently), where `v11` is an inline
    namespace, and `fmt::v10::formatter`, `fmt::v9::formatter`, and so on
    are also out in the wild.
    
    The next best thing is to write a class that has _all of the logic_ of
    what would be `fmt::formatter<Quantity<U, R>>`, without actually taking
    a physical dependency on the fmt library.  This actually does work, and
    this is what we do.  End users will need to make their own file which
    depends on Au, depends on fmt, and specializes `fmt::formatter` by just
    inheriting from `au::QuantityFormatter`.
    
    Now for the actual syntax.  I ended up going with two optional parts:
    one for the numeric value, and one for the unit label.  The unit label
    gets a `U` prefix, and we use a `;` to separate them if they're both
    there, but otherwise the syntax is simply "whatever syntax you would use
    for the underlying types".  This gives a lot of flexibility without a
    lot of complexity!
    
    I also added documentation explaining how to use the formatting support.
    
    Helps #149.  Really, this does _almost all_ of the work, but we can't
    actually test the `std::format` support with our current toolchains (as
    we're blocked on #485, #486, and #487).  So the plan is to land the
    support for `{fmt}`, and then play around on godbolt to validate our
    support for `std::format`.

---
+++ au/fmt_test.cc
@@ -0,0 +1,74 @@
+// Copyright 2025 Aurora Operations, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "au/prefix.hh"
+#include "au/quantity.hh"
+#include "au/units/meters.hh"
+#include "au/units/seconds.hh"
+#include "fmt/format.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace fmt {
+template <typename U, typename R>
+struct formatter<::au::Quantity<U, R>> : ::au::QuantityFormatter<U, R, ::fmt::formatter> {};
+}  // namespace fmt
+
+namespace au {
+namespace {
+
+using symbols::m;
+using symbols::s;
+constexpr auto cm = centi(m);
+constexpr auto km = kilo(m);
+
+using ::testing::StrEq;
+
+TEST(Fmt, PrintsQuantityAndUnitLabelByDefault) {
+    EXPECT_THAT(fmt::format("{}", meters(8.5)), StrEq("8.5 m"));
+}
+
+TEST(Fmt, DefaultFormatAppliesToNumberPart) {
+    EXPECT_THAT(fmt::format("{:,<10}", meters(8.5)), StrEq("8.5,,,,,,, m"));
+    EXPECT_THAT(fmt::format("{:,>10}", meters(8.5)), StrEq(",,,,,,,8.5 m"));
+    EXPECT_THAT(fmt::format("{:,>8.2f}", meters(0.1234)), StrEq(",,,,0.12 m"));
+}
+
+TEST(Fmt, CanFormatUnitLabelWithUPrefix) {
+    EXPECT_THAT(fmt::format("{:U,^5}", meters(8.5)), StrEq("8.5 ,,m,,"));
+}
+
+TEST(Fmt, CanFormatBothParts) {
+    // Overall width of 20: 10 for the number, 10 for the label.
+
+    EXPECT_THAT(fmt::format("{:*>10.3f;U,<10}", 123.456789 * cm / s),
+                StrEq("***123.457 cm / s,,,,"));
+}
+
+TEST(Fmt, DocExamplesAreCorrect) {
+    EXPECT_THAT(fmt::format("{}", meters(123.456)), StrEq("123.456 m"));
+    EXPECT_THAT(fmt::format("{:~^10.2f}", meters(123.456)), StrEq("~~123.46~~ m"));
+    EXPECT_THAT(fmt::format("{:U.>5}", meters(123.456)), StrEq("123.456 ....m"));
+    EXPECT_THAT(fmt::format("{:~^10.2f;U.>5}", meters(123.456)), StrEq("~~123.46~~ ....m"));
+
+    constexpr auto speed = (centi(meters) / second)(987.654321);
+    EXPECT_THAT(fmt::format("{:.^31}", fmt::format("{:,<8.2f;U*>10}", speed)),
+                StrEq("......987.65,, ****cm / s......"));
+
+    constexpr auto c = 299'792.458 * km / s;
+    EXPECT_THAT(fmt::format("{:,<12.2f}", c.data_in(km / s)), StrEq("299792.46,,,"));
+}
+
+}  // namespace
+}  // namespace au
