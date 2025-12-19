5073f909c9c13adb59be97871ad43898a5c79582
Michael Hordijk <hordijk@aurora.tech>
2025-12-19 07:16:45 -0500

    Get clang format working with the updated toolchains (#584)
    With the toolchain updates, we have a new repository name for the llvm
    toolchain.  Look for the "latest" llvm toolchain available when running
    clang-format.
    
    Update our tool wrapper to run under the current directory so paths
    passed in on the command line resolve correctly.
    
    Update the bazel output to suppress more output when building tools.
    
    Fixes #585.
    
    ---------
    
    Co-authored-by: Chip Hogg <charles.r.hogg@gmail.com>

--- tools/lib/command_from_bazel.sh
+++ tools/lib/command_from_bazel.sh
@@ -18,24 +18,36 @@ set -eou pipefail
 # In case users haven't set up `direnv`.
 export PATH="$PATH:./tools/bin"
 
-function wrap_bazel () {
-  COMMAND="$1"; shift
-  TARGET="$1"; shift
+function wrap_bazel() {
+  COMMAND="$1"
+  shift
 
-  bazel \
-    --nohome_rc \
-    "$COMMAND" \
-    --ui_event_filters=-info,-stdout,-stderr \
-    --noshow_progress \
-    "$TARGET" \
-    -- \
-    "$@"
+  TARGET="$1"
+  shift
+
+  bazel_opts_startup=(
+    "--nohome_rc"
+    "--quiet"
+  )
+
+  bazel_opts=(
+    "--ui_event_filters=-info,-stdout,-stderr"
+    "--noshow_progress"
+    "--run_under=cd $PWD &&"
+    "$TARGET"
+  )
+
+  bazel "${bazel_opts_startup[@]}" "$COMMAND" "${bazel_opts[@]}" -- "$@"
 }
 
-function make_command_from_bazel_run () {
-  BUILD_CMD="$1"; shift
-  TARGET="$1"; shift
-  USER_MESSAGE="Building tool.  If slow, you can Ctrl-C and run: bazel --nohome_rc $BUILD_CMD $TARGET"
+function make_command_from_bazel_run() {
+  BUILD_CMD="$1"
+  shift
+
+  TARGET="$1"
+  shift
+
+  USER_MESSAGE="Building tool.  If curious as to what's taking so long, you can Ctrl-C and run: bazel --nohome_rc $BUILD_CMD $TARGET"
 
   # Write message, then run _building_ command.
   # When done: back up; then, write spaces; then, back up again.
@@ -45,12 +57,20 @@ function make_command_from_bazel_run () {
 
   # Run _real_ command.
   if [ "$BUILD_CMD" == "fetch" ]; then
+    # With bzlmod, bazel fetch with a specific file in a module extension
+    # doesn't actually pull anything down, and query doesn't actually find it.
+    # With current versions of bazel, it looks like bazel build of the target
+    # file works, so try that.  Leaving this here for now in case I need to come
+    # back for it.
+    echo "ERROR: fetch with bzlmod may not work as expected..." >&2
+    exit 1
+
     # A target that is simply fetched is a pre-built target.  Bazel 6.0.0 will complain if we use
     # `bazel run`, because `bazel run` uses `bazel build` under the hood, and there is nothing to
     # build.  So in this case, we simply execute the target directly.
     QUERY_RESULT="$(bazel --nohome_rc query --noshow_progress $TARGET --output=location)"
     "${QUERY_RESULT%%:*}" "$@"
-  else
-    wrap_bazel run "$TARGET" "$@"
   fi
+
+  wrap_bazel run "$TARGET" "$@"
 }
