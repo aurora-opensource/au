8deea576a9553531073c7c2f9287ac4eee6d46d7
Chip Hogg <chogg@aurora.tech>
2026-01-08 13:41:46 -0500

    Extract Rankines as a first-class unit (#608)
    We had this buried inside of Fahrenheit since time immemorial, but
    that's just a weird situation.
    
    For symbols, I went with `degR` because the proper terminology is
    "degrees Rankine", and "R" is the most common label.  "Ra" is sometimes
    used to disambiguate with _even more obscure_ scales (Rømer and Réaumur)
    which I highly doubt that we will ever support.
    
    I did _not_ use the `_qty` suffix for the quantity maker (as for
    Fahrenheit and Celsius), because Rankines is more similar to Kelvins.
    
    As for naming, since we write `celsius` (not `celsiuses`) for "degrees
    Celsius", and `fahrenheit` (not `fahrenheits`) for "degrees Fahrenheit",
    I went with `rankine` (not `rankines`) as the stem.

--- au/units/rankine.hh
+++ au/units/rankine.hh
@@ -1,4 +1,4 @@
-// Copyright 2022 Aurora Operations, Inc.
+// Copyright 2024 Aurora Operations, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,10 +14,9 @@
 
 #pragma once
 
-#include "au/units/fahrenheit_fwd.hh"
+#include "au/units/rankine_fwd.hh"
 // Keep corresponding `_fwd.hh` file on top.
 
-#include "au/prefix.hh"
 #include "au/quantity.hh"
 #include "au/quantity_point.hh"
 #include "au/unit_symbol.hh"
@@ -27,33 +26,24 @@ namespace au {
 // DO NOT follow this pattern to define your own units.  This is for library-defined units.
 // Instead, follow instructions at (https://aurora-opensource.github.io/au/main/howto/new-units/).
 template <typename T>
-struct FahrenheitLabel {
-    static constexpr const char label[] = "degF";
+struct RankineLabel {
+    static constexpr const char label[] = "degR";
 };
 template <typename T>
-constexpr const char FahrenheitLabel<T>::label[];
-struct Fahrenheit
+constexpr const char RankineLabel<T>::label[];
+struct Rankine
     // In particular, do NOT manually specify `Dimension<...>` and `Magnitude<...>` types.  The
     // ordering of the arguments is very particular, and could change out from under you in future
     // versions, making the program ill-formed.  Only units defined within the Au library itself can
     // safely use this pattern.
     : UnitImpl<Temperature, Magnitude<Pow<Prime<3>, -2>, Prime<5>>>,
-      FahrenheitLabel<void> {
-    using FahrenheitLabel<void>::label;
-    static constexpr auto origin() {
-        // 459.67 Rankines = 45967 centi-rankines
-        return make_quantity<Centi<UnitImpl<Temperature, Magnitude<Pow<Prime<3>, -2>, Prime<5>>>>>(
-            45967);
-    }
+      RankineLabel<void> {
+    using RankineLabel<void>::label;
 };
-constexpr auto fahrenheit_qty = QuantityMaker<Fahrenheit>{};
-constexpr auto fahrenheit_pt = QuantityPointMaker<Fahrenheit>{};
-
-[[deprecated(
-    "`fahrenheit()` is ambiguous.  Use `fahrenheit_pt()` for _points_, or `fahrenheit_qty()` for "
-    "_quantities_")]] constexpr auto fahrenheit = QuantityMaker<Fahrenheit>{};
+constexpr auto rankine = QuantityMaker<Rankine>{};
+constexpr auto rankine_pt = QuantityPointMaker<Rankine>{};
 
 namespace symbols {
-constexpr auto degF_qty = SymbolFor<Fahrenheit>{};
+constexpr auto degR = SymbolFor<Rankine>{};
 }
 }  // namespace au
