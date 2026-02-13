105a114eacb03266720543206205334c9e431392
Chip Hogg <chogg@aurora.tech>
2026-02-12 20:12:23 -0500

    Bring AU to Au (#659)
    It's about time we added the Astronomical Unit!
    
    We'll use the slightly older "AU" symbol, rather than "au", to avoid
    confusion with our ubiquitous namespace.

---
+++ au/units/test/astronomical_units_test.cc
@@ -0,0 +1,37 @@
+// Copyright 2026 Aurora Operations, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "au/units/astronomical_units.hh"
+
+#include "au/testing.hh"
+#include "au/units/meters.hh"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace au {
+
+using ::testing::Eq;
+
+TEST(AstronomicalUnits, HasExpectedLabel) { expect_label<AstronomicalUnits>("AU"); }
+
+TEST(AstronomicalUnits, EquivalentTo149597870700Meters) {
+    EXPECT_THAT(astronomical_units(1), Eq(meters(149'597'870'700)));
+}
+
+TEST(AstronomicalUnits, HasExpectedSymbol) {
+    using symbols::AU;
+    EXPECT_THAT(5 * AU, SameTypeAndValue(astronomical_units(5)));
+}
+
+}  // namespace au
