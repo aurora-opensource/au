52c7444b5d848cf1da2bb945172d5792df729ada
Chip Hogg <chogg@aurora.tech>
2026-07-10 10:54:37 -0400

    Fix lint

--- au/quantity.hh
+++ au/quantity.hh
@@ -380,7 +380,9 @@ class Quantity {
         return make_quantity<decltype(pow<-1>(unit))>(s / a.value_);
     }
 
-    // Scaling by a Magnitude: scales the unit; for (m / q) we also take the reciprocal of q's stored value.
+    // Scaling by a Magnitude scales the unit.
+    //
+    // For (m / q) we also take the reciprocal of q's stored value.
     template <typename... BPs>
     friend AU_DEVICE_FUNC constexpr auto operator*(const Quantity &a, Magnitude<BPs...> m) {
         return make_quantity<decltype(unit * m)>(a.value_);
