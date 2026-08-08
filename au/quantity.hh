63f9dd48e9992a5492de93c049b722e01e9c5847
Chip Hogg <chogg@aurora.tech>
2026-08-08 13:58:59 -0400

    Revert "Stop using explicit rep in coerce functions"
    This reverts commit 2384af666f4b2bc7c32959898c47f4e78b46718c.

--- au/quantity.hh
+++ au/quantity.hh
@@ -245,22 +245,22 @@ class Quantity {
     template <typename NewUnit>
     constexpr auto coerce_as(NewUnit) const {
         // Usage example: `q.coerce_as(new_units)`.
-        return as(NewUnit{}, ignore(ALL_RISKS));
+        return as<Rep>(NewUnit{}, ignore(ALL_RISKS));
     }
     template <typename NewRep, typename NewUnit>
     constexpr auto coerce_as(NewUnit) const {
         // Usage example: `q.coerce_as<T>(new_units)`.
-        return as(NewUnit{}, ignore(ALL_RISKS));
+        return as<NewRep>(NewUnit{}, ignore(ALL_RISKS));
     }
     template <typename NewUnit>
     constexpr auto coerce_in(NewUnit) const {
         // Usage example: `q.coerce_in(new_units)`.
-        return in(NewUnit{}, ignore(ALL_RISKS));
+        return in<Rep>(NewUnit{}, ignore(ALL_RISKS));
     }
     template <typename NewRep, typename NewUnit>
     constexpr auto coerce_in(NewUnit) const {
         // Usage example: `q.coerce_in<T>(new_units)`.
-        return in(NewUnit{}, ignore(ALL_RISKS));
+        return in<NewRep>(NewUnit{}, ignore(ALL_RISKS));
     }
 
     // Direct access to the underlying value member, with any Quantity-equivalent Unit.
